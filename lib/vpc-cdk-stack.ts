c8a55af848df67873dedc8ed8565792e25da2746
Craig Mullins <craigmul@amazon.com>
2022-08-18 12:37:31 +0200

    Creating VPC with 2 Private subnets && 2 Public Subnets

--- lib/vpc-cdk-stack.ts
+++ lib/vpc-cdk-stack.ts
@@ -1,13 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+//import * as iam from 'aws-cdk-lib/aws-iam'
 
 export class VpcCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    // Create new VPC with 2 Subnets
+    const vpc = new ec2.Vpc(this, 'DMS-VPC', {
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "dms",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED
+        },
+        {
+          cidrMask: 24,
+          name: "public",
+          subnetType: ec2.SubnetType.PUBLIC
+        }
+      ]
+    });
 
+    // Allow SSH (TCP Port 22) access from anywhere
+    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc,
+      description: 'Allow SSH (TCP port 22) in',
+      allowAllOutbound: true
+    });
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH Access')
+
+// Create outputs for connecting
+    new cdk.CfnOutput(this, 'VPC Arn', { value: vpc.vpcArn });
     // example resource
     // const queue = new sqs.Queue(this, 'VpcCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
